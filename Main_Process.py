17fb9069469d4cc4c7775e0bd84f664a7623a3de
TowlGol <2909291649@qq.com~>
2025-02-26 20:21:15 +0800

    update Main_Process

--- Main_Process.py
+++ Main_Process.py
@@ -4,9 +4,10 @@ Path = "examples/"
 Output_Path = "examples/"
 # split name by ","
 #    Dataset 1:
-fileName = "B1.pdb,B2.pdb,B3.pdb,B4.pdb,B5.pdb,B6.pdb,B7.pdb,B8.pdb,B9.pdb,B10.pdb,B11.pdb,B12.pdb,B13.pdb"
+# fileName = "B1.pdb,B2.pdb,B3.pdb,B4.pdb,B5.pdb,B6.pdb,B7.pdb,B8.pdb,B9.pdb,B10.pdb,B11.pdb,B12.pdb,B13.pdb"
 #    Dataset 2:
 # fileName = "A1.pdb,C1.pdb,F1.pdb,F2.pdb,H1.pdb,N1.pdb,O1.pdb,O2.pdb,W1.pdb"
+fileName = "O2.pdb"
 # 1.Centroid 2.Center of Mass 3.Symmetrical Point
 ball_center_type = 2
 # recommend 3 or 4
