da7eadc4507d58abcaa7f42fe2d549e75f0d2331
TowlGol <2909291649@qq.com~>
2025-03-13 15:45:17 +0800

    Update

--- Main_Process.py
+++ Main_Process.py
@@ -8,8 +8,7 @@ fileName = "B1.pdb,B2.pdb,B3.pdb,B4.pdb,B5.pdb,B6.pdb,B7.pdb,B8.pdb,B9.pdb,B10.p
 #    Dataset 2:
 # fileName = "A1.pdb,C1.pdb,F1.pdb,F2.pdb,H1.pdb,N1.pdb,O1.pdb,O2.pdb,W1.pdb"
 # 1.Centroid 2.Center of Mass 3.Symmetrical Point
-fileName = "O2.pdb"
 ball_center_type = 2
-# recommend 3 or 4
+# recommend 3, 4, and 5
 subdivision_time = 4
 cav.Calculate_Cavity(fileName, ball_center_type, subdivision_time,Path,Output_Path)
